a8507fc31aa4f2ffc70918374bdf03705c9f578f
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2019-04-15 10:22:49 -0300

    Aula 3 - Entendendo Polimorfismo

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,10 @@
+
+public class EditorVideo extends Funcionario {
+
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do Editor de Video");
+		return super.getBonificacao() + 100;
+
+	}
+
+}
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -16,6 +16,7 @@ public class Gerente extends Funcionario {
 	}
 
 	public double getBonificacao() {
+		System.out.println("Chamando o método bonificacao do GERENTE");
 		return super.getBonificacao() + super.getSalario();
 	}
 
